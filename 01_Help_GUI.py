b21d46f3656d9f665f14276e5d7d2a01fbd81b42
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-02-05 10:36:04 +1300

    Part of component 1 is present however an error has occured

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -1,15 +1,30 @@
 from tkinter import *
+from functools import partial  # To prevent unwanted windows
+
 import random
 
 
-class Convertor:
-    def __init__(self, parent):
-        print("hello world")
+class Converter:
+    def __init__(self):
+
+        # Formatting variables...
+        background_color = "light brown"
+
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame.grid()
+
+        # Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
 
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("Temperature Convertor")
-    something = Convertor(root)
+    root.title("Temperature Converter")
+    something = Converter()
     root.mainloop()
