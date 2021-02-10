d2de62c56ac316192d12d5e0f86674fb2bf83ce7
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-02-10 13:15:02 +1300

    Started temp checking component
    (in the process of number checking / rounding)

---
+++ 06_number_checker_v1.py
@@ -0,0 +1,10 @@
+# This component is used to check that a users entered number is within given paramaters
+
+
+# Temp Number Checker function
+def temp_check(low):
+    valid = False
+
+
+# main routine
+
