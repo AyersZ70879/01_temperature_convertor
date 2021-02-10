57001233d87ffd02c3a78528eac6b9d46ce1a2ab
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-02-10 13:19:19 +1300

    Finished component 6 - Number checker works as expected

--- 06_number_checker_v1.py
+++ 06_number_checker_v1.py
@@ -4,7 +4,22 @@
 # Temp Number Checker function
 def temp_check(low):
     valid = False
+    while not valid:
+        try:
+            response = float(input("Enter a number: "))
 
+            if response < low:
+                print("Too Cold!")
+
+            else:
+                return response
+
+        except ValueError:
+            print("Please enter a number")
 
 # main routine
+number = temp_check(-273)
+print("You chose {}".format(number))
 
+# number = temp_check(-459)
+# print("You chose {}".format(number))
