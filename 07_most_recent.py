6e9995d18d6de430a1b96319c66581b74c3344f9
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-02-11 12:18:29 +1300

    Eodw - 2:19 through Top 3 entries

---
+++ 07_most_recent.py
@@ -0,0 +1,19 @@
+# Get the data from the user and store it in a list then display the most recent three entries
+
+# Set up the empty lists
+all_calculations = []
+
+# Get five items of Data
+for item in range (0, 5):
+    get_item = input("Enter an item: ")
+    all_calculations.append(get_item)
+
+# Show that everything made it to the list..
+print()
+print("*The Full List*")
+print(all_calculations)
+
+print()
+
+print("*** The Most Recent 3 ***")
+# print items starting at the END of the list
