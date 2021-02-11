bc4ebfa6b4687d6250099c058c0e65040c529ebe
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-02-12 10:09:42 +1300

    Final changes made to 07 - Video finished (3 entries)

--- 07_most_recent.py
+++ 07_most_recent.py
@@ -4,17 +4,34 @@
 all_calculations = []
 
 # Get five items of Data
-for item in range (0, 5):
+get_item = ""
+while get_item != "xxx":
     get_item = input("Enter an item: ")
+
+    if get_item == "xxx":
+        break
+
     all_calculations.append(get_item)
 
-# Show that everything made it to the list..
-print()
-print("*The Full List*")
-print(all_calculations)
-
 print()
 
-print("*** The Most Recent 3 ***")
-# print items starting at the END of the list
-print()
+if len(all_calculations) == 0:
+    print("Oops - the list is empty")
+else:
+    # Show that everything made it to the list..
+    print()
+    print("*The Full List*")
+    print(all_calculations)
+
+    print()
+
+    # print items starting at the END of the list
+    if len(all_calculations) >= 3:
+        print("*** The Most Recent 3 ***")
+
+        for item in range(0, 3):
+            print(all_calculations[len(all_calculations) - item - 1])
+    else:
+        print("*** Items from Newest to Oldest ***")
+        for item in all_calculations:
+            print(all_calculations[len(all_calculations) - all_calculations.index(item)])
